14fc4d9f4c978f359e6002165fa2da1f3157783d
inesouarghi <ines.ouarghi@isen.yncrea.fr>
2025-04-27 12:25:29 +0200

    bug bandit

--- home/apps.py
+++ home/apps.py
@@ -9,5 +9,7 @@ if __name__ == "__main__":
     from urls import register_routes
     app = Flask(__name__)
     register_routes(app)
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="127.0.0.1", port=8000)
+
+
 
